02b1ee60c00f07e0e04e2e5f3c199efe16dffee8
WonJin <try3982@kakao.com>
2025-09-21 19:55:05 +0900

    -. 예외처리

--- src/main/java/com/singleton/exception/ErrorCode.java
+++ src/main/java/com/singleton/exception/ErrorCode.java
@@ -13,9 +13,13 @@ import static org.springframework.http.HttpStatus.BAD_REQUEST;
 @AllArgsConstructor
 public enum ErrorCode {
 
-    USE_Not_FOUNDO("존재하지 않는 사용자입니다.",BAD_REQUEST),
+    USER_NOT_FOUND("존재하지 않는 사용자입니다.",BAD_REQUEST),
     DUPICATE_EMAIL("이미 사용중인 이메일입니다.",BAD_REQUEST),
     INVALID_LOGIN("이메일 또는 비밀번호가 올바르지 않습니다",BAD_REQUEST),
+    POST_NOT_FOUND("게시글을 찾을 수 없습니다.",BAD_REQUEST),
+    AUTHORIZATION_FAILED("작성자를 찾을 수 없습니다.",BAD_REQUEST),
+    COMMENT_NOT_FOUND("댓글을 찾을 수 없습니다.",BAD_REQUEST),
+    UTHORIZATION_FAILED("다른 사용자의 댓글은 수정/삭제할 수 없습니다.",BAD_REQUEST),
 
 
 
